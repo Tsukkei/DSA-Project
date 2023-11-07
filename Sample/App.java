3d9dea1295517f5c86b3bb77239f45e170a2374a
Earl Anthony Gamboa <111488396+Tsukkei@users.noreply.github.com>
2023-11-08 02:36:41 +0800

    Update App.java

--- Sample/App.java
+++ Sample/App.java
@@ -1,31 +1,28 @@
-import java.util.PriorityQueue;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        PriorityQueue<String> songQueue = new PriorityQueue<>(10, (s1, s2) -> s1.compareTo(s2));
+        ArrayList<String> songList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
-        // Initialize the priority queue with some default songs
-        songQueue.add("Lihim by Arthur Miguel");
-        songQueue.add("Standing next to you by JK (BTS)");
-        songQueue.add("Unang Sayaw by Nobita");
-        songQueue.add("Mercy by Shawn Mendez");
-        songQueue.add("Fleeting Lullaby by Ado");
-        songQueue.add("Gusto feat. Al James by Zack Tabudlo, Al James");
-        songQueue.add("Who by Lauv, BTS");
-        songQueue.add("Future Perfect by ENHYPEN");
-        songQueue.add("Pasilyo by Sunkissed Lola");
-        songQueue.add("Raining in Manila by Lola Amour");
-
+        // Initialize the list with some default songs
+        songList.add("Lihim by Arthur Miguel" );
+        songList.add("Standing next to you by JK (BTS)" );
+        songList.add("Unang Sayaw by Nobita" );
+        songList.add("Mercy by Shawn Mendez" );
+        songList.add("Fleeting Lullaby by Ado" );
+        songList.add("Gusto feat.Al James by Zack Tabudlo, Al James");
+        songList.add("Who by Lauv, BTS" );
+        songList.add("Future Perfect by ENHYPEN" );
+        songList.add("Pasilyo by Sunkissed Lola" );
+        songList.add("Raining in Manila by Lola Amour" );
+        // ... Add more songs
         while (true) {
             System.out.println("Current Playlist:");
             System.out.println("   ");
-
-            // Convert the priority queue to an array for display
-            String[] songArray = songQueue.toArray(new String[0]);
-            for (int i = 0; i < songArray.length; i++) {
-                System.out.println((i + 1) + ". " + songArray[i]);
+            for (int i = 0; i < songList.size(); i++) {
+                System.out.println((i + 1) + ". " + songList.get(i));
             }
 
             System.out.println("   ");
@@ -46,24 +43,13 @@ public class App {
                 case 1:
                     System.out.print("Enter the name of your favorite song: ");
                     String favoriteSong = scanner.nextLine();
-                    songQueue.add(favoriteSong);
+                    songList.add(favoriteSong);
                     break;
                 case 2:
                     System.out.print("Enter the index of the song to remove: ");
                     int indexToRemove = scanner.nextInt();
-                    if (indexToRemove >= 1 && indexToRemove <= songArray.length) {
-                        // Convert the array back to a priority queue to remove elements
-                        songQueue = new PriorityQueue<>(songArray.length, (s1, s2) -> s1.compareTo(s2));
-                        for (String song : songArray) {
-                            if (song != null) {
-                                songQueue.add(song);
-                            }
-                        }
-                        // Remove the selected song
-                        for (int i = 1; i < indexToRemove; i++) {
-                            songQueue.poll();
-                        }
-                        songQueue.poll(); // Remove the selected song
+                    if (indexToRemove >= 1 && indexToRemove <= songList.size()) {
+                        songList.remove(indexToRemove - 1);
                     } else {
                         System.out.println("Invalid index.");
                     }
@@ -74,20 +60,8 @@ public class App {
                     scanner.nextLine(); // Consume the newline character
                     System.out.print("Enter the new song: ");
                     String newSong = scanner.nextLine();
-                    if (indexToReplace >= 1 && indexToReplace <= songArray.length) {
-                        // Convert the array back to a priority queue to replace elements
-                        songQueue = new PriorityQueue<>(songArray.length, (s1, s2) -> s1.compareTo(s2));
-                        for (String song : songArray) {
-                            if (song != null) {
-                                songQueue.add(song);
-                            }
-                        }
-                        // Replace the selected song
-                        for (int i = 1; i < indexToReplace; i++) {
-                            songQueue.poll();
-                        }
-                        songQueue.poll(); // Remove the selected song
-                        songQueue.add(newSong); // Add the new song
+                    if (indexToReplace >= 1 && indexToReplace <= songList.size()) {
+                        songList.set(indexToReplace - 1, newSong);
                     } else {
                         System.out.println("Invalid index.");
                     }
